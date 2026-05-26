d843c7ba1830f8481a3bfa79ce998ba47f6d0d74
Pitts1204 <briandpitts@gmail.com>
2026-05-26 10:58:07 -0500

    Add checklist managers for epcot-day3

--- scripts/shared.js
+++ scripts/shared.js
@@ -192,6 +192,7 @@ document.addEventListener("DOMContentLoaded", () => {
   createChecklistManager("pack-list", "pack-input", "pack-add-btn", "trip-pack");
   createChecklistManager("prep-list", "prep-input", "prep-add-btn", "mk-day2-prep");
   createChecklistManager("nextday-list", "nextday-input", "nextday-add-btn", "mk-day2-nextday");
-
+  createChecklistManager("prep-list", "prep-input", "prep-add-btn", "epcot-day3-prep");
+  createChecklistManager("nextday-list", "nextday-input", "nextday-add-btn", "epcot-day3-nextday");
 });
 
